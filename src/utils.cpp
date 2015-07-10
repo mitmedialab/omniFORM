f41f8960e907c7c11ab2ff853a79bf5054ef8850
Daniel Windham <dewindham@gmail.com>
2015-07-10 13:44:51 -0400

    utils: add elapsed time util

---
+++ src/utils.cpp
@@ -0,0 +1,14 @@
+//
+//  utils.cpp
+//  TMG Shape Displays
+//
+//  Created by Daniel Windham on 4/28/15.
+//  Copyright 2010 MIT Media Lab. All rights reserved.
+//
+
+#include "utils.h"
+
+
+double elapsedTimeInSeconds() {
+    return clock() / (double) CLOCKS_PER_SEC;
+}
---
+++ src/utils.h
@@ -0,0 +1,15 @@
+//
+//  utils.h
+//  TMG Shape Displays
+//
+//  Created by Daniel Windham on 4/28/15.
+//  Copyright 2010 MIT Media Lab. All rights reserved.
+//
+
+#pragma once
+
+#include <ctime>
+#include "ofMain.h"
+
+
+double elapsedTimeInSeconds();
