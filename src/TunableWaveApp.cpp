3d6f6d242cc51d27390d76c50b250a233e912a6f
lukevink <luke@lukevink.com>
2015-07-14 16:48:54 -0400

    tWave: Added spatial context ctrl to freq + wav #

--- src/TunableWaveApp.cpp
+++ src/TunableWaveApp.cpp
@@ -61,8 +61,49 @@ void TunableWaveApp::updateHeights() {
 };
 
 void TunableWaveApp::updateWaveParametersWithKinect() {
-    frequency = 1.0;
-    numCrests = 1.0;
+    // Get Pixels from kinect
+    ofPixels depthPixels;
+    kinectManager->getDepthPixels(depthPixels);
+    int tableMaskLine = KINECT_Y - 200;
+    int closestY = -1;
+    int closestX = KINECT_X / 2;
+
+//     Mask out all pixels before y Baseline
+//    for (int y = 0; y < tableMaskLine; y++) {
+//        for (int x = 0; x < KINECT_X; x++) {
+//            depthPixels[x + (y * KINECT_X)] = 0;
+//        }
+//    }
+    
+    // Get cloest Y
+    for (int y = tableMaskLine; y >= 0; y--) {
+        for (int x = 0; x < KINECT_X; x++) {
+            if (depthPixels[x + (y * KINECT_X)] > 0){
+                closestY = y;
+                closestX = x;
+                break;
+            }
+        }
+        if (closestY > -1) {
+            break;
+        }
+    }
+
+    float dropoffRate = 4;
+    float minFreq = 0.5;
+    float maxFreq = 5.5;
+    
+    // Compare pixel distance from Y baseline (table), set freqeuncy
+    if (closestY > -1){
+        float normalized = 1.0 * (tableMaskLine - closestY) / tableMaskLine;
+        float k = pow(2.8f, -dropoffRate + 2 * dropoffRate * normalized);
+        frequency = minFreq + (maxFreq - minFreq) * k / (1 + k);
+    } else {
+        frequency = maxFreq;
+    }
+    
+    // Get the closest pixel and use this as the control
+    numCrests = 10.0 * closestX / KINECT_X;
 };
 
 void TunableWaveApp::drawGraphicsForShapeDisplay() {
